ffc18700cd41d12f450f91143989220c670c4867
AnshitaKanthed <anshita.j08@gmail.com>
2024-01-24 21:53:38 +0530

    24 january

---
+++ 1742A.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if((a+b==c) || (a+c==b) || (b+c==a)){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+
+    return 0;
+}
